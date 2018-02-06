2645bfd5bf3e774fb91475c5a05d76cd0b4dc4e2
yoshi-PC\yoshi <nagase2@gmail.com>
2018-02-06 23:34:22 +0900

    修正

--- spec/headless.spec.js
+++ spec/headless.spec.js
@@ -10,34 +10,56 @@ var fs = require('fs');
 
 'use strict'
 
-
 describe('Zaif Manager', () => {
   //テストで毎回実施するやり方は？
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000
+  const width = 800;
+  const height = 1000;
+
+  let driver
   beforeEach(function () {
-
-
-  });
-
-  it('ticker情報からbidが取得できている', (done) => {
-
-    const width = 800;
-    const height = 1000;
-
-    let driver = new Builder()
+    driver = new Builder()
       .forBrowser('chrome')
       .setChromeOptions(
       new chrome.Options().headless().windowSize({ width, height }))
-      // .setFirefoxOptions(
-      //     new firefox.Options().headless().windowSize({width, height}))
+      .setFirefoxOptions(
+      new firefox.Options().headless().windowSize({ width, height }))
       .build();
+  });
 
+
+  it('ticker情報からbidが取得できている', (done) => {
     driver.get('http://www.google.com/ncr')
       .then(_ => {
         driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
         //console.log(By.name('q'))
       })
-      .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 1000)).then(_ => {
+      .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 2000)).then(_ => {
+        driver.getTitle().then(title => console.log(title/*?*/))
+      }).then(_ => driver.takeScreenshot().then(
+        function (image, err) {
+          //Screenshot will be saved under current directory with name myscreenshot.png
+          fs.writeFile('./myscreenshot.png', image, 'base64', function (error) {
+            if (error != null) {
+              console.log("Error occured while saving screenshot" + error);
+            } else {
+              done()
+            }
+          });
+        })
+      )
+      .then(
+      _ => driver.quit(),
+      e => driver.quit().then(() => { throw e; }));
+  });
+
+  it('ticker情報からbidが取得できている2', (done) => {
+    driver.get('http://www.google.com/ncr')
+      .then(_ => {
+        driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+        //console.log(By.name('q'))
+      })
+      .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 2000)).then(_ => {
         driver.getTitle().then(title => console.log(title/*?*/))
       }).then(_ => driver.takeScreenshot().then(
         function (image, err) {
@@ -56,6 +78,13 @@ describe('Zaif Manager', () => {
       e => driver.quit().then(() => { throw e; }));
 
   });
+
+  afterEach(function () {
+    driver = null
+
+  })
+
+
 
 
 
