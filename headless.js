7d95c154e55b5371aa0d5e82e68f057b09ce348b
yoshimasa nagase <yoshimasa_nagase@tsyscom.co.jp>
2018-02-06 20:35:22 +0900

    動作確認済み

---
+++ headless.js
@@ -0,0 +1,26 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let chrome = require('selenium-webdriver/chrome');
+let firefox = require('selenium-webdriver/firefox');
+
+const width = 640;
+const height = 480;
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(
+        new chrome.Options().headless().windowSize({width, height}))
+    .setFirefoxOptions(
+        new firefox.Options().headless().windowSize({width, height}))
+    .build();
+
+driver.get('http://www.google.com/ncr')
+    .then(_ =>{
+        driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+        console.log(By.name('q'))
+    }
+    )
+    .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
+    .then(
+
+        _ => driver.quit(),
+        e => driver.quit().then(() => { throw e; }));
